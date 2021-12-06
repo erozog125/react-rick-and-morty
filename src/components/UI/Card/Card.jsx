c86249596f82f8ec69a448a308335579b6e3d706
legarrod <luis.garcia@atlanticsoft.us>
2021-12-06 07:48:56 -0500

    ejemplo traer informacion al dar enter

--- src/components/UI/Card/Card.jsx
+++ src/components/UI/Card/Card.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from 'react';
 import './Card.css';
 
-export const Card = ({card}) => {
-
+export const Card = ({ card }) => {
   const { name, image, gender } = card;
   return (
     <>
-    <div className="card">
-      <div className="div-img">
-        <img className="img-card" src={image} alt={name} />      
+      <div className="card">
+        <div className="div-img">
+          <img className="img-card" src={image} alt={name} />
+        </div>
+        <div className="txt-content">
+          <h2>{name}</h2>
+          <p>Gender:{gender}</p>
+        </div>
       </div>
-      <div className="txt-content">
-        <h2>{name}</h2>
-        <p>Gender:{gender}</p>
-      </div>
-    </div>    
-    </>    
-  )
-}
+    </>
+  );
+};
