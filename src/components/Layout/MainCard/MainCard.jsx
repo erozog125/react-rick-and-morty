ceea87eb53c01e5bc7821f57b528abad68516cf3
Edwin Rozo Gómez <erozog125@gmail.com>
2021-12-05 21:00:03 -0500

    Buscando como renderizar todas las cards

--- src/components/Layout/MainCard/MainCard.jsx
+++ src/components/Layout/MainCard/MainCard.jsx
@@ -4,32 +4,29 @@ import { UILookingCharacter } from '../../UI/UILookingCharacter/UILookingCharact
 import image1 from '../../../Images/banner2.png';
 
 export const MainCard = () => {
-
-  // const card = {name:"Rick",gender:"Male",image:image1};
+  
   const [card, setCard] = useState({name:"Rick",gender:"Male",image:image1});
-  const URL = 'https://rickandmortyapi.com/api/character';
+  const URL = 'https://rickandmortyapi.com/api/character';  
+  let cards = [];
 
-  function renderCards(event) {
-    let input_character = event.target.value;
-    if (event.keyCode === 13) {
+  const FetchApi=(event)=>{
+    if (event.keyCode === 13) {      
       fetch(URL)
-      .then(response=>response.json())
+      .then(res=>res.json())
       .then(data=>{
-        data.results.map(element=>{
-          if (card.name.includes(input_character)) {
-            setCard(element);
-          }
-        })
+        cards = [...cards,data.results];
+        setCard(data.results[2])
       })
     }
   }
 
+    
   return (
     <>
-      <UILookingCharacter placeHolder="Find a character" event={renderCards} />
+      <UILookingCharacter placeHolder="Find a character" event={FetchApi} />
       <main>
-        <Card card={card} />
-      </main>            
+        <Card card={card} />           
+      </main>
     </>
   )
 }
--- src/components/Page/Home/Home.jsx
+++ src/components/Page/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { MainCard } from '../../Layout/MainCard/MainCard'
 // import { Header } from '../../Layout/Header/Header';
 // import { LookingCharacter } from '../../Layout/LookingCharacter/LookingCharacter';
-import { MainCard } from '../../Layout/MainCard/MainCard';
+
 
 export const Home = () => {
 
